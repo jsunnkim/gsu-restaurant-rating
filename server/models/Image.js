9f1aa7d99a024951ec7fee900a2352f4481b4b82
jsunnkim <jsunnkim@gmail.com>
2025-05-01 17:32:43 -0400

    image

--- server/models/Image.js
+++ server/models/Image.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose')
+
 const ImageSchema = new mongoose.Schema({
   src: String,
   alt: String,
   author: String,
-  password: String,  // ← 이 필드가 반드시 있어야 해!
+  password: String, // ✅ 사용자 입력 비밀번호 저장
   createdAt: { type: Date, default: Date.now }
 })
+
 module.exports = mongoose.model('Image', ImageSchema)
