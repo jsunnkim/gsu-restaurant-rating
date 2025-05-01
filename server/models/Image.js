c83b6c2b774991e1caa32e8378756a49e381740b
jsunnkim <jsunnkim@gmail.com>
2025-05-01 17:29:28 -0400

    imagejs

--- server/models/Image.js
+++ server/models/Image.js
@@ -1,8 +1,9 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 const ImageSchema = new mongoose.Schema({
   src: String,
   alt: String,
   author: String,
+  password: String,  // ← 이 필드가 반드시 있어야 해!
   createdAt: { type: Date, default: Date.now }
-});
-module.exports = mongoose.model('Image', ImageSchema);
+})
+module.exports = mongoose.model('Image', ImageSchema)
